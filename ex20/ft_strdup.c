20d17fb04ceec1c5231d1bb372b666d60d7dc035
Romulo Do Nascimento Santos <romdo-na@c2r3s10.42porto.com>
2026-04-16 20:07:46 +0100

    ex20

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,11 +6,41 @@
 /*   By: romdo-na <romdo-na@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 18:27:34 by romdo-na          #+#    #+#             */
-/*   Updated: 2026/04/16 18:28:23 by romdo-na         ###   ########.fr       */
+/*   Updated: 2026/04/16 20:07:16 by romdo-na         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strdup(char *src)
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strdup(char *src)
 {
-    
+	int		len;
+	char	*str_copy;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (src[i])
+	{
+		len++;
+		i++;
+	}
+	str_copy = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (src[i])
+	{
+		str_copy[i] = src[i];
+		i++;
+	}
+	str_copy[i] = '\0';
+	return (str_copy);
 }
+
+// int main()
+// {
+//     char *s1 = "hello";
+//     char *s2 = ft_strdup(s1);
+
+//     printf("%s\n", s2);
+// }
