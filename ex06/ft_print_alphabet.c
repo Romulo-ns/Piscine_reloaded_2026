9bb9a47675035eb55d69472e7a79e2077f3e011a
Romulo Do Nascimento Santos <romdo-na@c2r3s10.42porto.com>
2026-04-16 20:30:18 +0100

    fix

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,20 +6,25 @@
 /*   By: romdo-na <romdo-na@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 16:52:59 by romdo-na          #+#    #+#             */
-/*   Updated: 2026/04/16 19:43:24 by romdo-na         ###   ########.fr       */
+/*   Updated: 2026/04/16 20:29:45 by romdo-na         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_print_alphabet(void)
 {
 	char	l;
 
-	l = 97;
-	while (l <= 122)
+	l = 'a';
+	while (l <= 'z')
 	{
-		write(1, &l, 1);
+		ft_putchar(l);
 		l++;
 	}
 }
